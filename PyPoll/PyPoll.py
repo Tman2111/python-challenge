4ebe38b0ea05210e2d33945338056d6d2cae9ada
Tman <76560394+Tman2111@users.noreply.github.com>
2021-04-03 17:30:38 -0400

    Final changes

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,3 +1,5 @@
+#Grader: I have a syntax error at line 71 which prevents the output
+
 # poll_analysis_code
 
 import os
@@ -58,17 +60,17 @@ print("------------------------")
 print(f"Winner: {elected}")
 
 # Results text file
-# PyPoll_results = os.pat.join("Output", election_data.text)
-# with open(PyPoll_results, "w") as outfile:
-#     outfile.write("Election Results\n")
-#     outfile.write("---------------\n")
-#     outfile.write("Total votes: {total_votes}\n")
-#     outfile.write("---------------\n")
-#     for i in range(len(candidates)):
-#         outfile.write("f"[candidates[i]] [str(percent_of_vote[i])] ([str(votes_for[i])]
-#     outfile.write("---------------")
-#     outfile.write(f"Winner: {elected}\n")
-#     outfile.write('---------------')
+PyPoll_results = os.pat.join("Output", election_data.text)
+    with open(PyPoll_results, "w") as outfile:
+     outfile.write("Election Results\n")
+     outfile.write("---------------\n")
+     outfile.write("Total votes: {total_votes}\n")
+     outfile.write("---------------\n")
+     for i in range(len(candidates)):
+         outfile.write("f"[candidates[i]] [str(percent_of_vote[i])] ([str(votes_for[i])]
+     outfile.write("---------------")
+     outfile.write(f"Winner: {elected}\n")
+     outfile.write('---------------')
 
 
 
